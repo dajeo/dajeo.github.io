3a1a5f1944c7d029d5655f990971c5542ff724f6
User3 <headcrabj1@gmail.com>
2023-02-16 00:48:03 +0500

    Add meta http-equiv

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,9 @@ export default defineNuxtConfig({
             charset: "utf-8",
             viewport: "width=device-width, initial-scale=1.0",
             title: "Headcrab",
+            meta: [
+                { "http-equiv": "Permissions-Policy", content: "interest-cohort=()" }
+            ],
             link: [
                 { href: "https://fonts.googleapis.com", rel: "preconnect" },
                 { href: "https://fonts.gstatic.com", rel: "preconnect", crossorigin: "" },
