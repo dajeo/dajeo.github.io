096e702aa163d6cfa04f35ca1845a32a55797c1f
Danil Chernyshov <headcrabj1@gmail.com>
2023-03-31 06:23:35 +0000

    Add links

--- src/components/HomeBlock.vue
+++ src/components/HomeBlock.vue
@@ -1,58 +1,30 @@
 <template>
-  <div class="container">
-    <div class="img-container">
-      <img class="img" src="../assets/man.png" alt="Man" />
-    </div>
-    <div class="content">
-      <h1 class="content-title">Hi, I'm Danil.</h1>
-      <p>
-        I'm a young <b>software engineer</b>. Develop <b>high-load</b> services in Golang. Analysis and optimizing code.
-      </p>
-      <div>
-        <a href="https://github.com/dajeo">GitHub</a>
-        <a href="https://t.me/dajeo">Telegram</a>
-      </div>
-    </div>
+  <h1 class="content-title">Hi, I'm Danil.</h1>
+  <p>
+    I'm a young <b>software engineer</b>. Develop <b>high-load</b> services in Golang. Analysis and optimizing code.
+  </p>
+  <div class="refs">
+    <a href="https://github.com/dajeo">GitHub</a>
+    <a href="https://t.me/dajeo">Telegram</a>
+    <a href="https://gitlab.com/dajeo">GitLab</a>
+    <a href="https://twitter.com/dajeo_danil">Twitter</a>
+    <a href="https://discord.com/users/1090900421284999178">Discord</a>
+    <a href="https://habr.com/en/users/dajeo/">Habr</a>
   </div>
 </template>
 
 <style scoped>
-.container {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-}
-
-.img-container {
-  text-align: center;
-}
-
-.img {
-  width: 400px;
-}
-
-@media (max-width: 700px) {
-  .container {
-    grid-template-columns: 1fr;
-    margin-bottom: 100px;
-  }
-
-  .img {
-    width: 350px;
-  }
-}
-
-.content {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
 .content-title {
   margin: 0;
 }
 
 p {
   max-width: 340px;
+}
+
+.refs {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 6px;
 }
 </style>
