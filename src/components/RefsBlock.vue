873a62dc19e64cc41f3734e8f462cc38c2c1542d
Danil Chernyshov <headcrabj1@gmail.com>
2023-04-10 08:38:23 +0000

    Add links

--- src/components/RefsBlock.vue
+++ src/components/RefsBlock.vue
@@ -6,6 +6,11 @@
     <a href="https://twitter.com/dajeo_danil">Twitter</a>
     <a href="https://discord.com/users/1090900421284999178">Discord</a>
     <a href="https://habr.com/en/users/dajeo/">Habr</a>
+    <a href="https://app.daily.dev/dajeo">Daily.dev</a>
+    <a href="https://www.youtube.com/@dajeo">YouTube</a>
+    <a href="https://dev.to/dajeo">Dev.to</a>
+    <a href="https://hashnode.com/@dajeo">Hashnode</a>
+    <a href="https://mastodon.social/@dajeo">Mastodon</a>
   </div>
 </template>
 
